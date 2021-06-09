f048afcd28fbd7709fc803aedc3de2410025d802
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2021-06-09 21:17:35 +0200

    add medicos, controller, routes, model

---
+++ models/medico.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const MedicoSchema = Schema({
+    nombre: {
+        type: String,
+        require: true,
+    },
+    img: {
+        type: String,
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+}, { collection: 'medicos' });
+
+MedicoSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+});
+
+module.exports = model('Medico', MedicoSchema);
---
+++ routes/medicos.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const { getMedicos, addMedico, updateMedico, deleteMedico } = require('../controllers/medicos.controller');
+
+const router = Router();
+/**
+ * /api/medicos
+ */
+
+router.get('/', [], getMedicos);
+router.post('/', [], addMedico);
+router.put('/:id', [], updateMedico);
+router.delete('/:id', [], deleteMedico)
+
+module.exports = router;
