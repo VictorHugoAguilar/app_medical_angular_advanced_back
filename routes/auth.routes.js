7b11779a1c2aa15dca6bd2c68ae14b84e730d5c5
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2021-06-14 20:33:12 +0200

    add refresh token controller and routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
-const { login, googleSignIn } = require('../controllers/auth.controller');
+const { login, googleSignIn, refreshToken } = require('../controllers/auth.controller');
 
 const router = Router();
 /**
  * Rutas: /api/login
  */
+router.get('/', [
+    validarJWT
+], refreshToken);
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').not().isEmpty(),
@@ -17,7 +21,6 @@ router.post('/google', [
     check('token', 'El token de google es obligatorio').not().isEmpty(),
     validarCampos
 ], googleSignIn);
-
 
 
 module.exports = router;
